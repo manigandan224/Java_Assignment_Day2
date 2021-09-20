a1915f36140b62c565fafce6b7e7341f4bc493d4
manigandan224 <65449581+manigandan224@users.noreply.github.com>
2021-09-20 19:53:11 +0530

    Add files via upload

---
+++ Day2/command_line_argument.java
@@ -0,0 +1,9 @@
+class commandline{
+public static void main(String args[])
+{
+int x = Integer.parseInt(args[0]);
+int y = Integer.parseInt(args[1]);
+int sum = x+y;
+System.out.println("Sum="+sum);
+}
+}
